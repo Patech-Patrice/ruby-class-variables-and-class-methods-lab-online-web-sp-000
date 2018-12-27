d5a5e00e60ec3fb0f4bdcb040e1a880339cd078d
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-27 17:45:37 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+   attr_accessor :name, :artist, :genre
+   @@genre = []
+   @@count= 0
+  @@artists = []
+  
+   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@artists << artist
+    @@genres << genre
+    
+   end
+   
+   def self.count
+     @@count
+   end
+   
+  end 
